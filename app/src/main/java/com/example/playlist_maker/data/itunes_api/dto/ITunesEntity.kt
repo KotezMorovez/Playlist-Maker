953a067945d9731a3c22bc43ce0e050ea5dd643d
KotezMorovez <odevyatlovskaya@gmail.com>
2024-09-17 11:44:12 +0500

    Fix issues

--- app/src/main/java/com/example/playlist_maker/data/itunes_api/dto/ITunesEntity.kt
+++ app/src/main/java/com/example/playlist_maker/data/itunes_api/dto/ITunesEntity.kt
@@ -2,7 +2,6 @@ package com.example.playlist_maker.data.itunes_api.dto
 
 import com.example.playlist_maker.data.prefs.dto.TrackEntity
 
-// FixMe: класс был создан и размещен до рефакторинга
 data class ITunesEntity(
     val resultCount: Int,
     val results: List<TrackEntity>
--- app/src/main/java/com/example/playlist_maker/data/prefs/dto/TrackDataMapper.kt
+++ app/src/main/java/com/example/playlist_maker/data/prefs/dto/TrackDataMapper.kt
@@ -1,19 +1,23 @@
 package com.example.playlist_maker.data.prefs.dto
 
 import com.example.playlist_maker.data.itunes_api.dto.ITunesEntity
-import com.example.playlist_maker.domain.prefs.dto.Track
 import com.example.playlist_maker.domain.itunes_api.dto.TrackList
+import com.example.playlist_maker.domain.prefs.dto.Track
 
 fun ITunesEntity.toDomain(): TrackList {
     return TrackList(
         resultCount = this.resultCount,
-        tracks = this.results.map {
+        tracks = this.results.mapNotNull {
             it.toDomain()
         }
     )
 }
 
-fun TrackEntity.toDomain(): Track {
+fun TrackEntity.toDomain(): Track? {
+    if (previewUrl == null) {
+        return null
+    }
+
     return Track(
         trackId = this.trackId,
         trackName = this.trackName,
